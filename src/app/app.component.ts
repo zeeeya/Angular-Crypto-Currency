6a0f022cc5f25bbb092ff5f3f5a67dacfa528e58
zeeeya <zci.alvarez@gmail.com>
2018-02-28 10:27:59 +0800

    Added import and defined 2 properties in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  objectKeys = Object.keys;
+  cryptos: any;
+  
+  // other code removed for brevity
+
 }
