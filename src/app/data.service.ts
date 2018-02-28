c91a0523d01deed379d1194654b9b8ae0b7bbfd1
zeeeya <zci.alvarez@gmail.com>
2018-02-28 10:26:02 +0800

    Added new import in data.service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  result:any;
+
+  constructor(private _http: HttpClient) {}
+
+  getPrices() {
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD")
+      .map(result => this.result = result);
+  }
 
 }
